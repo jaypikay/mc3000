27b83ec4671a752fade4020f84ab2780af10da10
Julian Knauer <jpk@goatpr0n.de>
2017-10-10 09:34:31 +0200

    Monitor scripts writes an report

--- mc3k.py
+++ mc3k.py
@@ -18,6 +18,7 @@ from mc3000 import MC3000
 from mc3000rrd import *
 
 
+RPT_NAME = 'MC3000-{date}-Report.txt'
 RRD_NAME = 'MC3000-{date}-Slot{index}.rrd'
 PNG_NAME = 'MC3000-{date}-Slot{index}.png'
 
@@ -27,11 +28,22 @@ if __name__ == '__main__':
 
     print('Preparing RRDs for battery slots...')
     timestamp = int(time.time())
-    for i, battery in enumerate(mc3k.battery_data):
-        print('  - Slot #{index}: {mode} {battery}'.format(index=i+1, mode=battery.mode,
+
+    rptfile = open(RPT_NAME.format(date=timestamp), 'w')
+
+    for battery in mc3k.battery_data:
+        print('  - Slot #{index}: {mode} {battery}'.format(index=battery.slot+1,
+                                                           mode=battery.mode,
                                                            battery=battery.type))
-        create_rrd(RRD_NAME.format(index=i+1, date=timestamp),
-                   timestamp)
+        create_rrd(RRD_NAME.format(index=battery.slot+1, date=timestamp), timestamp)
+
+    rptfile.write('Charger Report\n\nStart Time: {date}\nBatteries:\b'.format(date=timestamp))
+    for battery in mc3k.get_charging_progress():
+        if battery.voltage > 0:
+            rptfile.write(' - Battery in Slot #{slot}\n'.format(slot=battery.slot+1))
+            rptfile.write('   Voltage: {voltage}\n'.format(voltage=battery.voltage))
+            rptfile.write('   Temperature: {bat_tem}\n'.format(bat_tem=battery.bat_tem))
+    rptfile.write('\n')
 
     try:
         print('Recording charging progress...')
@@ -54,11 +66,23 @@ if __name__ == '__main__':
     finally:
         print('Terminating...')
         mc3k.close()
+    end_ts = int(time.time())
+
+    rptfile.write('Charging summary for Batteries:\n')
+    rptfile.write('End Time: {date}'.format(end_ts))
+    for battery in mc3k.get_charging_progress():
+        if battery.voltage > 0:
+            rptfile.write(' - Battery in Slot #{slot}\n'.format(slot=battery.slot+1))
+            rptfile.write('   Voltage: {voltage}\n'.format(voltage=battery.voltage))
+            rptfile.write('   Time: {time}\n'.format(time=battery.work_time))
+            rptfile.write('   Temperature: {bat_tem}\n'.format(bat_tem=battery.bat_tem))
+            rptfile.write('   Inner resistance: {ir}\n'.format(ir=battery.inner_resistance))
+    rptfile.close()
 
     print('Preparing graphs for battery slots...')
-    ts = int(time.time())
-    for i, battery in enumerate(mc3k.battery_data):
-        print('  - Slot #{index}: Rendering graph...'.format(index=i+1))
-        graph_rrd(PNG_NAME.format(index=i+1, date=timestamp),
-                  RRD_NAME.format(index=i+1, date=timestamp),
-                  timestamp, ts)
+    for battery in mc3k.battery_data:
+        print('  - Slot #{index}: Rendering graph...'.format(index=battery.slot+1))
+        graph_rrd(PNG_NAME.format(index=battery.slot+1, date=timestamp),
+                  RRD_NAME.format(index=battery.slot+1, date=timestamp),
+                  timestamp, end_ts)
+    print('Finished.')
