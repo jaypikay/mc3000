c94237d3202f7c5c99e74f1510a22b3b44bafb3b
Julian Knauer <jpk@goatpr0n.de>
2017-10-19 18:31:34 +0200

    Verbose output added to execute_rrd_cmd

--- mc3000rrd.py
+++ mc3000rrd.py
@@ -39,7 +39,7 @@ AREA:batcurrent#0000FF80:Current \
 LINE1:batbat_tem#00FF00:Temperature"""
 
 
-def execute_rrd_cmd(command_line):
+def execute_rrd_cmd(command_line, verbose_output=False):
     """Run *command_line* as arguments for **rrdtool** command.
 
     Command is executed as subprocess in backgroud.
@@ -47,21 +47,22 @@ def execute_rrd_cmd(command_line):
     :param command_line: command line arguments for *rrdtool update*
     :type data_packet: str
     """
-    print(command_line)
+    if verbose_output:
+        print(command_line, file=sys.stderr)
     subprocess.run(command_line.split(' '), stderr=subprocess.DEVNULL)
 
 
-def create_rrd(filename, starttime):
+def create_rrd(filename, starttime, verbose_output=False):
     """Create a new RRD with *filename* and start collecting data since *starttime* timestamp.
 
     :param filename: Filename of the RRD file to be created
     :type filename: str
     """
     cmd = RRD_CREATE.format(filename=filename, start=starttime)
-    execute_rrd_cmd(cmd)
+    execute_rrd_cmd(cmd, verbose_output)
 
 
-def update_rrd(filename, battery):
+def update_rrd(filename, battery, verbose_output=False):
     """Build the command line parameters with values to be stored in *filename* for each *battery*.
 
     :param filename: Filename of the RRD file to be updated
@@ -73,10 +74,10 @@ def update_rrd(filename, battery):
                             voltage=int(float(battery['voltage']) * 1000),
                             current=int(float(battery['current']) * 1000),
                             bat_tem=int(float(battery['bat_tem']) * 10))
-    execute_rrd_cmd(cmd)
+    execute_rrd_cmd(cmd, verbose_output)
 
 
-def graph_rrd(png_file, rrd_file, starttime, endtime):
+def graph_rrd(png_file, rrd_file, starttime, endtime, verbose_output=False):
     """Render a PNG image with the values stored in *rrd_file* within the timestamp range of
     *starttime* and *endtime*. The resulting image will by stored with the filename *png_file*.
 
@@ -91,22 +92,4 @@ def graph_rrd(png_file, rrd_file, starttime, endtime):
     """
     cmd = RRD_GRAPH.format(png_file=png_file, rrd_file=rrd_file,
                            ts_start=starttime, ts_end=endtime)
-    execute_rrd_cmd(cmd)
-
-
-if __name__ == '__main__':
-    try:
-        filename = sys.argv[1]
-    except IndexError:
-        print('Not enough parameters.\n  Usage: {} <csv>\n'.format(sys.argv[0]))
-        sys.exit(1)
-    with open(filename, 'r') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=' ', quotechar='|')
-        for i, row in enumerate(reader):
-            if i == 0:
-                ts_start = row['ts']
-                rrd_file = 'MC3000-{}-Battery_0.rrd'.format(ts_start)
-                create_rrd(rrd_file, row['ts'])
-            update_rrd(rrd_file, row)
-            ts = row['ts']
-        graph_rrd('MC3000-{}-Battery_0.png'.format(ts_start), rrd_file, ts_start, ts)
+    execute_rrd_cmd(cmd, verbose_output)
